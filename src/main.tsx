ea610b958df81e5a2ac757641171a602728dce44
PrzemyslawTrabuc <czemek17@gmail.com>
2022-08-20 17:47:07 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { store } from './store/store';
 import { Provider } from 'react-redux';
 import { HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>   
       <HashRouter>
       <Provider store={store}>  
