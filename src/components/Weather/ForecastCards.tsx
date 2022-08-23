905cf98659cef08de0a88ec9a24c6dc07b590666
przemyslawtrabuc <czemek17@gmail.com>
2022-08-23 15:26:10 +0200

    Update ForecastCards.tsx
    test ghDesktop

--- src/components/Weather/ForecastCards.tsx
+++ src/components/Weather/ForecastCards.tsx
@@ -1,4 +1,4 @@
-import React, {MouseEventHandler, useEffect, useRef} from "react";
+import React, { MouseEventHandler, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import type { RootState } from "../../store/store";
 import {
@@ -17,39 +17,40 @@ import WeatherIcon from "../Weather/WeatherIcon";
 import metersPerSecToKilometersPerH from "../../utils/metersPerSecToKilometersPerH";
 
 const ForecastCards = (props: any) => {
-    const isModalOpen = useSelector((state: RootState) => state.Modal.isOpen);
-    const isDarkModeOn = useSelector((state: RootState) => state.DarkModeSwitch.isDarkModeOn);
-    const theme = useMantineTheme();
-    const scrollDivRef = useRef<any>();
-    const forecastScrollListPosition = useRef({left:0, x:0});
+  const isModalOpen = useSelector((state: RootState) => state.Modal.isOpen);
+  const isDarkModeOn = useSelector(
+    (state: RootState) => state.DarkModeSwitch.isDarkModeOn
+  );
+  const theme = useMantineTheme();
+  const scrollDivRef = useRef<any>();
+  const forecastScrollListPosition = useRef({ left: 0, x: 0 });
 
-    const mouseDownHandler = (e:React.MouseEvent)=> {
-      if(scrollDivRef.current){
+  const mouseDownHandler = (e: React.MouseEvent) => {
+    if (scrollDivRef.current) {
       forecastScrollListPosition.current = {
         left: scrollDivRef.current.scrollLeft,
-        x: e.clientX
-      }  
-      document.addEventListener('mousemove', mouseMoveHandler);
-      document.addEventListener('mouseup', mouseUpHandler);
-      scrollDivRef.current.style.cursor="grabbing";
+        x: e.clientX,
+      };
+      document.addEventListener("mousemove", mouseMoveHandler);
+      document.addEventListener("mouseup", mouseUpHandler);
+      scrollDivRef.current.style.cursor = "grabbing";
     }
   };
 
-  const mouseMoveHandler = function (e:MouseEvent) {
+  const mouseMoveHandler = function (e: MouseEvent) {
     // How far the mouse has been moved
     const dx = e.clientX - forecastScrollListPosition.current.x;
 
-    if(scrollDivRef.current)
-    scrollDivRef.current.scrollLeft = forecastScrollListPosition.current.left - dx;
-   
-};
-console = console
-const mouseUpHandler = function () {
-  document.removeEventListener('mousemove', mouseMoveHandler);
-  document.removeEventListener('mouseup', mouseUpHandler);
-  if(scrollDivRef.current)
-  scrollDivRef.current.style.cursor="grab";
-};
+    if (scrollDivRef.current)
+      scrollDivRef.current.scrollLeft =
+        forecastScrollListPosition.current.left - dx;
+  };
+  console = console;
+  const mouseUpHandler = function () {
+    document.removeEventListener("mousemove", mouseMoveHandler);
+    document.removeEventListener("mouseup", mouseUpHandler);
+    if (scrollDivRef.current) scrollDivRef.current.style.cursor = "grab";
+  };
 
   const buildDaysForecastList = (numberOfCards: number) => {
     const reducedForecast = props.forecastData.list.slice(0, numberOfCards);
@@ -63,7 +64,13 @@ const mouseUpHandler = function () {
           margin: 8,
         }}
       >
-        <Card style={{minWidth:200, backgroundColor: isModalOpen && !isDarkModeOn  ? theme.colors.gray[0] : "" }}>
+        <Card
+          style={{
+            minWidth: 200,
+            backgroundColor:
+              isModalOpen && !isDarkModeOn ? theme.colors.gray[0] : "",
+          }}
+        >
           <Center>
             <Stack>
               <Title align="center" order={4} color="#8c8c8c">
@@ -76,23 +83,27 @@ const mouseUpHandler = function () {
                 </Title>
               </Center>
               <Group spacing="sm" grow>
-                <CloudRain size={30} color={isDarkModeOn ? theme.colors.cyan[8] : theme.colors.cyan[6]}/>
-                <Title color="cyan" order={4}>{Math.round(item.pop * 100)} %</Title>
-                <Cloud size={25} />
-                <Title order={5}>
-                    {item.clouds.all} %
+                <CloudRain
+                  size={30}
+                  color={
+                    isDarkModeOn ? theme.colors.cyan[8] : theme.colors.cyan[6]
+                  }
+                />
+                <Title color="cyan" order={4}>
+                  {Math.round(item.pop * 100)} %
                 </Title>
+                <Cloud size={25} />
+                <Title order={5}>{item.clouds.all} %</Title>
               </Group>
-                <Group>
-                    <Windsock size={25} />
-                    <Title order={5}>
-                        {metersPerSecToKilometersPerH(item.wind.speed).toFixed(2)} km/h         
-                    </Title>
-                    <DropletFilled2 size={25} />
-                    <Title order={5}>
-                        {item.main.humidity} %                
-                    </Title>
-                </Group>
+              <Group>
+                <Windsock size={25} />
+                <Title order={5}>
+                  {metersPerSecToKilometersPerH(item.wind.speed).toFixed(2)}{" "}
+                  km/h
+                </Title>
+                <DropletFilled2 size={25} />
+                <Title order={5}>{item.main.humidity} %</Title>
+              </Group>
             </Stack>
           </Center>
         </Card>
@@ -103,12 +114,22 @@ const mouseUpHandler = function () {
   if (Object.keys(props.forecastData).length > 0)
     return (
       <>
-        <List style={{ whiteSpace: "nowrap",width:"95%", marginLeft:"auto", marginRight:"auto", cursor:"grab"}}>
+        <List
+          style={{
+            whiteSpace: "nowrap",
+            width: "95%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            cursor: "grab",
+          }}
+        >
           <Center>
-            <ScrollArea viewportRef={scrollDivRef} offsetScrollbars style={{ height: 300}}>
-              <div 
-                onMouseDown={mouseDownHandler}
-              >
+            <ScrollArea
+              viewportRef={scrollDivRef}
+              offsetScrollbars
+              style={{ height: 300 }}
+            >
+              <div onMouseDown={mouseDownHandler}>
                 {buildDaysForecastList(40)}
               </div>
             </ScrollArea>
