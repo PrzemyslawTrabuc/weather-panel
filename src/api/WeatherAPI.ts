cdd45e62bc84b08da97576d7a1ede3592d1df6f4
przemyslawtrabuc <czemek17@gmail.com>
2024-01-22 22:19:42 +0100

    cleanup

--- src/api/WeatherAPI.ts
+++ src/api/WeatherAPI.ts
@@ -1,4 +1,4 @@
-const apiKey: string = "e443ff5717f5a87ef95c3333d0945a2f";
+const apiKey: string = import.meta.env.VITE_API_WEATHER;
 const baseUrl: string = `https://api.openweathermap.org/data/2.5/`;
 const weatherImageBaseUrl: string = "https://openweathermap.org/img/wn/";
 
